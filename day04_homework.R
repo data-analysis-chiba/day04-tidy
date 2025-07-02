14c5394bf408e899c50f59f2e41d8e130e130e7c
joelnitta <joelnitta@gmail.com>
2025-07-02 09:41:40 +0900

    Update HW to be compatible with {gradeR}

--- day04_homework.R
+++ day04_homework.R
@@ -3,26 +3,28 @@
 # Name:
 
 
-# 1. Write code to load the "gapminder" dataset (note that this repo comes
-# with the gapminder.csv file in the data_raw folder):
-
-
-# 2. What is the country with the highest GDP per capita (gdpPercap)?
+# 1. Load the "gapminder" dataset and save it to an object called gapminder
 #
-# Answer:
+# HINT: This repo includes the gapminder.csv file in the data_raw folder
+# HINT: I've written some of the code for you.
 
-# 3. Write the code that you used to answer #2:
+gapminder <-
+
+# 2. Sort the data so that the country with the highest per capita GDP
+# (gdpPercap) is at the top. Save the result to answer_2
 
 
-# 4. Write code to filter the gapminder data to the most recent year in the
+# 3. Filter the gapminder data to the most recent year in the
 # dataset, then select only the country and population columns. DO NOT use the
-# pipe for this.
+# pipe for this. Save the result to answer_3
 
 
-# 5. Answer question 4 again, but this time USE the pipe (%>%).
+# 4. Answer question 4 again, but this time USE the pipe (|>). Save the
+# result to answer_4
 
 
-# 6. Write code to calculate the mean life expectancy for each country in
-# Asia only.
+# 5. Calculate the mean life expectancy for each country in
+# Asia only. Save the result to answer_5
+#
 # HINT: use the combination of `group_by()` and `summarize()` to calculate
 # a mean value by group
