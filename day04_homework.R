3e8ea74f918259d8ee54e83ace6586f1463ba028
joelnitta <joelnitta@gmail.com>
2025-07-09 12:51:15 +0900

    Add instructions to load packages

--- day04_homework.R
+++ day04_homework.R
@@ -5,28 +5,27 @@
 # For this week's homework, the answer to each question should be a dataframe
 # (tibble)
 
-# 1. Load the "gapminder" dataset and save it to an object called gapminder
+# 1. Load the data from gapminder.csv and save it to an object called gapminder
 #
 # HINT: This repo includes the gapminder.csv file in the data_raw folder
-# HINT: I've written some of the code for you.
-
-gapminder <-
-
-# 2. Sort the data so that the country with the highest per capita GDP
-# (gdpPercap) is at the top. Save the result to answer_2
+# HINT: You need to first load the tidyverse package before doing anything else
 
 
-# 3. Filter the gapminder data to the most recent year in the
-# dataset, then select only the country and population columns. DO NOT use the
-# pipe for this. Save the result to answer_3
+# 2. Sort the gapminder data so that the country with the highest per capita GDP
+#    (gdpPercap) is at the top. Save the result to answer_2
 
 
-# 4. Answer question 4 again, but this time USE the pipe (|>). Save the
-# result to answer_4
+# 3. Filter the gapminder data to the most recent year in the dataset, then
+#    select only the country and population columns. DO NOT use the pipe for
+#    this. Save the result to answer_3
 
 
-# 5. Calculate the mean life expectancy for each country in
-# Asia only. Save the result to answer_5
+# 4. Answer question 4 again, but this time USE the pipe (|>). Save the result
+#    to answer_4
+
+
+# 5. Calculate the mean life expectancy for each country in Asia only. Save the
+#    result to answer_5
 #
 # HINT: use the combination of `group_by()` and `summarize()` to calculate
 # a mean value by group
